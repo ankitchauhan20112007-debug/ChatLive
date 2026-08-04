c52ad54044c6f994ffa39a527515cdc459ce9761
ankitchauhan20112007-debug <ankit.chauhan.20112007@gmail.com>
2026-08-04 23:12:32 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,20 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 
-import {
-  getAuth,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
-import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQW8TYSFy1G6cXeGyYyscnWnh9Kqk5g6o",
+  apiKey: "YOUR_API_KEY",
   authDomain: "chatlive-bac03.firebaseapp.com",
   projectId: "chatlive-bac03",
   storageBucket: "chatlive-bac03.firebasestorage.app",
@@ -23,83 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 
-const profilePic = document.getElementById("profilePic");
-const profileImage = document.getElementById("profileImage");
-const uploadBtn = document.getElementById("uploadBtn");
-const userName = document.getElementById("userName");
-const userEmail = document.getElementById("userEmail");
-const logout = document.getElementById("logout");
-
-onAuthStateChanged(auth, async (user) => {
-
-  if (!user) {
-    window.location.href = "index.html";
-    return;
-  }
-
-  userName.textContent = user.displayName || "ChatLive User";
-  userEmail.textContent = user.email;
-
-  const snap = await getDoc(doc(db, "users", user.uid));
-
-  if (snap.exists()) {
-    const data = snap.data();
-
-    if (data.photo) {
-      profilePic.src = data.photo;
-    }
-  }
-
-});uploadBtn.onclick = async () => {
-  try {
-
-    if (!profileImage.files[0]) {
-      alert("Please select an image");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("file", profileImage.files[0]);
-    formData.append("upload_preset", "swlqxqgn");
-
-    const response = await fetch(
-      "https://api.cloudinary.com/v1_1/rmt792pr/image/upload",
-      {
-        method: "POST",
-        body: formData
-      }
-    );
-
-    const data = await response.json();
-
-    if (!data.secure_url) {
-      alert("Upload Failed");
-      console.log(data);
-      return;
-    }
-
-    profilePic.src = data.secure_url;
-
-    await setDoc(
-      doc(db, "users", auth.currentUser.uid),
-      {
-        photo: data.secure_url
-      },
-      { merge: true }
-    );
-
-    alert("Profile photo uploaded successfully!");
-
-  } catch (err) {
-    console.log(err);
-    alert("Error: " + err.message);
-  }
-};
-
-logout.onclick = async () => {
-  await signOut(auth);
-  window.location.href = "index.html";
-};
+export const auth = getAuth(app);
+export const db = getFirestore(app);
