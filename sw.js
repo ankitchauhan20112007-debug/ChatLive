bef0e6f25552680477e25a5a56ebfa92e91a7dbc
ankitchauhan20112007-debug <ankit.chauhan.20112007@gmail.com>
2026-08-04 12:47:29 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const cacheName = "chatlive-v1";
+
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
