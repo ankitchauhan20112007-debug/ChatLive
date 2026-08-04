798e9ef601deeff88ae158a14b2db8933db19c5b
ankitchauhan20112007-debug <ankit.chauhan.20112007@gmail.com>
2026-08-04 15:47:53 +0530

    Update post.js

--- post.js
+++ post.js
@@ -10,8 +10,12 @@ import {
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCQW8TYFy1G6cXeGyYyscnWnh9Kqk5g6o",
+  apiKey: "AIzaSyCQW8TYSFy1G6cXeGyYyscnWnh9Kqk5g6o",
   authDomain: "chatlive-bac03.firebaseapp.com",
   projectId: "chatlive-bac03",
   storageBucket: "chatlive-bac03.firebasestorage.app",
@@ -21,54 +25,122 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
+const postImage = document.getElementById("postImage");
 const caption = document.getElementById("caption");
 const postBtn = document.getElementById("postBtn");
 const posts = document.getElementById("posts");
 
-
 postBtn.onclick = async () => {
 
-  if(caption.value.trim() === "") return;
+  if (!postImage.files[0]) {
+    alert("Please select an image");
+    return;
+  }
 
-  await addDoc(collection(db,"posts"),{
-    text: caption.value,
+  const formData = new FormData();
+  formData.append("file", postImage.files[0]);
+  formData.append("upload_preset", "swlqxqgn");
+
+  const res = await fetch(
+    "https://api.cloudinary.com/v1_1/rmt792pr/image/upload",
+    {
+      method: "POST",
+      body: formData
+    }
+  );
+
+  const data = await res.json();
+
+  await addDoc(collection(db, "posts"), {
+    image: data.secure_url,
+    caption: caption.value,
+    email: auth.currentUser.email,
     time: serverTimestamp()
   });
 
   caption.value = "";
+  postImage.value = "";
 
+  alert("Post Uploaded!");
 };
 
-
-// Show Posts
-
 const q = query(
-  collection(db,"posts"),
-  orderBy("time","desc")
+  collection(db, "posts"),
+  orderBy("time", "desc")
 );
 
+onSnapshot(q, (snapshot) => {
 
-onSnapshot(q,(snapshot)=>{
+  posts.innerHTML = "";
 
-  posts.innerHTML="";
-
-  snapshot.forEach((doc)=>{
+  snapshot.forEach((doc) => {
 
     const data = doc.data();
 
-    posts.innerHTML += `
-    <div style="
-    background:white;
-    padding:15px;
-    margin:10px;
-    border-radius:10px;
-    ">
-    
-    📝 ${data.text}
+   posts.innerHTML += `
+<div style="
+background:#fff;
+margin:15px 0;
+border-radius:15px;
+overflow:hidden;
+box-shadow:0 2px 8px rgba(0,0,0,.15);
+">
 
-    </div>
-    `;
+<div style="
+display:flex;
+align-items:center;
+padding:12px;
+">
+
+<img src="${data.image}"
+style="
+width:45px;
+height:45px;
+border-radius:50%;
+object-fit:cover;
+margin-right:10px;
+">
+
+<div>
+
+<b>${data.email}</b>
+
+<br>
+
+<small style="color:gray;">
+ChatLive
+</small>
+
+</div>
+
+</div>
+
+<img
+src="${data.image}"
+style="
+width:100%;
+display:block;
+max-height:500px;
+object-fit:cover;
+">
+
+<div style="padding:12px;">
+
+<div style="font-size:24px;">
+❤️ 🤍 💬 📤
+</div>
+
+<p style="margin-top:10px;">
+<b>${data.email}</b>
+${data.caption}
+</p>
+
+</div>
+
+</div>
+`;
 
   });
 
