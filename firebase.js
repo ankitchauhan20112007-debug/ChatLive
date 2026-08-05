5b29e4b6d30195f91e37355474c835977b10054a
ankitchauhan20112007-debug <ankit.chauhan.20112007@gmail.com>
2026-08-05 11:45:47 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -11,7 +11,20 @@ import {
   getFirestore,
   doc,
   setDoc
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";const name = document.getElementById("name");
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyCQW8TYSFy1G6cXeGyYyscnWnh9Kqk5g6o",
+  authDomain: "chatlive-bac03.firebaseapp.com",
+  projectId: "chatlive-bac03",
+  storageBucket: "chatlive-bac03.firebasestorage.app",
+  messagingSenderId: "1097708548558",
+  appId: "1:1097708548558:web:32a13c6cf0b624a2eafb9f"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const name = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const signup = document.getElementById("signup");
