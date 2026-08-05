a5a8a7a18a1546d365207e9282bf5b181abd25a5
ankitchauhan20112007-debug <ankit.chauhan.20112007@gmail.com>
2026-08-05 11:56:25 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,31 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { auth, db } from "./firebase.js";
 
 import {
-  getAuth,
   onAuthStateChanged,
   signOut
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 import {
-  getFirestore,
   collection,
   doc,
   setDoc,
   onSnapshot
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCQW8TYSFy1G6cXeGyYyscnWnh9Kqk5g6o",
-  authDomain: "chatlive-bac03.firebaseapp.com",
-  projectId: "chatlive-bac03",
-  storageBucket: "chatlive-bac03.firebasestorage.app",
-  messagingSenderId: "1097708548558",
-  appId: "1:1097708548558:web:32a13c6cf0b624a2eafb9f"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 
 const usersDiv = document.getElementById("users");
 
