a5f74e666c9378c5f8e985e821fa24aefd9c5f00
ankitchauhan20112007-debug <ankit.chauhan.20112007@gmail.com>
2026-08-05 11:35:18 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 import {
   getAuth,
@@ -12,51 +7,48 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCQW8TYSFy1G6cXeGyYyscnWnh9Kqk5g6o",
-  authDomain: "chatlive-bac03.firebaseapp.com",
-  projectId: "chatlive-bac03",
-  storageBucket: "chatlive-bac03.firebasestorage.app",
-  messagingSenderId: "1097708548558",
-  appId: "1:1097708548558:web:32a13c6cf0b624a2eafb9f"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const name = document.getElementById("name");
+import {
+  getFirestore,
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";const name = document.getElementById("name");
+const email = document.getElementById("email");
 const password = document.getElementById("password");
 const signup = document.getElementById("signup");
 const login = document.getElementById("login");
 const msg = document.getElementById("msg");
+
 onAuthStateChanged(auth, (user) => {
   if (user) {
     window.location.href = "home.html";
   }
-});
-
-signup.onclick = async () => {
+});signup.onclick = async () => {
   try {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email.value,
-      password.value
+
+    const userCredential =
+      await createUserWithEmailAndPassword(
+        auth,
+        email.value,
+        password.value
+      );
+
+    await setDoc(
+      doc(db, "users", userCredential.user.uid),
+      {
+        uid: userCredential.user.uid,
+        name: name.value,
+        email: email.value
+      }
     );
 
-   await setDoc(doc(db, "users", userCredential.user.uid), {
-  uid: userCredential.user.uid,
-  name: name.value,
-  email: email.value
-});
-
     window.location.href = "home.html";
+
   } catch (error) {
     msg.innerHTML = error.message;
   }
-};
-
-login.onclick = async () => {
+};login.onclick = async () => {
   try {
+
     await signInWithEmailAndPassword(
       auth,
       email.value,
@@ -64,6 +56,7 @@ login.onclick = async () => {
     );
 
     window.location.href = "home.html";
+
   } catch (error) {
     msg.innerHTML = error.message;
   }
