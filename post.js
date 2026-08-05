0ffea11f14461a746879c6411b6d0fd478e2fee2
ankitchauhan20112007-debug <ankit.chauhan.20112007@gmail.com>
2026-08-05 11:13:28 +0530

    Update post.js

--- post.js
+++ post.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+
 
 import {
   getFirestore,
@@ -13,18 +13,7 @@ import {
   increment
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCQW8TYSFy1G6cXeGyYyscnWnh9Kqk5g6o",
-  authDomain: "chatlive-bac03.firebaseapp.com",
-  projectId: "chatlive-bac03",
-  storageBucket: "chatlive-bac03.firebasestorage.app",
-  messagingSenderId: "1097708548558",
-  appId: "1:1097708548558:web:32a13c6cf0b624a2eafb9f"
-};import { auth, db } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 
 const postImage = document.getElementById("postImage");
 const caption = document.getElementById("caption");
