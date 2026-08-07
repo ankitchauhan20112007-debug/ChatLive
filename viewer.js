6bfedc4dca896f98814a96666c29d5d7db26d1a7
ankitchauhan20112007-debug <ankit.chauhan.20112007@gmail.com>
2026-08-07 13:12:56 +0530

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -1,55 +1,15 @@
-import { db } from "./firebase.js";
-
-import {
-  collection,
-  query,
-  orderBy,
-  onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
-
 const storyImg = document.getElementById("storyImg");
 
-const q = query(
-  collection(db, "stories"),
-  orderBy("time", "desc")
-);
+const savedStory = localStorage.getItem("storyImage");
 
-let stories = [];
-let index = 0;
-
-onSnapshot(q, (snapshot) => {
-
-  stories = [];
-
-  snapshot.forEach((doc) => {
-    stories.push(doc.data());
-  });
-
- if (stories.length > 0) {
-
-  const savedStory = localStorage.getItem("storyImage");
-
-  if (savedStory) {
-    storyImg.src = savedStory;
-  } else {
-    storyImg.src = stories[0].image;
-  }
-
+if (savedStory) {
+  storyImg.src = savedStory;
+} else {
+  alert("No Story Found");
+  window.location.href = "home.html";
 }
 
-});
-
-setInterval(() => {
-
-  if (stories.length === 0) return;
-
-  index++;
-
-  if (index >= stories.length) {
-    window.location.href = "home.html";
-    return;
-  }
-
-  storyImg.src = stories[index].image;
-
+// 5 सेकंड बाद Home पर वापस
+setTimeout(() => {
+  window.location.href = "home.html";
 }, 5000);
