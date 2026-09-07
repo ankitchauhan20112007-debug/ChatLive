334816118a15e5133ca8c27a97f3823241d7554f
ankitchauhan20112007-debug <ankit.chauhan.20112007@gmail.com>
2026-09-07 16:56:49 +0530

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,34 +1,5 @@
-const CLOUD_NAME = "YOUR_CLOUD_NAME";
-const UPLOAD_PRESET = "YOUR_UPLOAD_PRESET";
-
-export async function uploadToCloudinary(file) {
-
-  const url =
-    `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
-
-  const formData = new FormData();
-
-  formData.append("file", file);
-  formData.append("upload_preset", UPLOAD_PRESET);
-
-  const response = await fetch(url, {
-    method: "POST",
-    body: formData
-  });
-
-  if (!response.ok) {
-    throw new Error("Cloudinary upload failed");
-  }
-
-  const data = await response.json();
-
-  if (!data.secure_url) {
-    throw new Error("Photo URL नहीं मिली");
-  }
-
-  return data.secure_url;
-}const CLOUD_NAME = "YOUR_CLOUD_NAME";
-const UPLOAD_PRESET = "YOUR_UPLOAD_PRESET";
+const CLOUD_NAME = "rmt792pr";
+const UPLOAD_PRESET = "chatlive";
 
 export async function uploadToCloudinary(file) {
 
